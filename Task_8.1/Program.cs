09e7f903fb0b6613d57548f5a9e430082d37948e
Максим Белов <makbe@yandex.ru>
2021-11-24 23:50:31 +0300

    Добавьте файлы проекта.

---
+++ Task_8.1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task_8._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите путь к каталогу для вывода его содержимого:");
+            string path = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Содержимое указанного каталога:");
+            string[] filesParentDir = Directory.GetFiles(path);
+            foreach (string f in filesParentDir)
+            {
+                Console.WriteLine(f);
+            }
+            string[] subDirectories = Directory.GetDirectories(path, "*", SearchOption.AllDirectories);
+            foreach (string i in subDirectories)
+            {
+                Console.WriteLine(i);
+                string[] files =  Directory.GetFiles(i);
+                foreach (string f in files)
+                {
+                    Console.WriteLine(f);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
